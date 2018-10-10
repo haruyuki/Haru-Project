c3c2b91e9239428ec9b54d8e880175190c724bc8
haruyuki <jumpy12359@gmail.com>
2018-10-10 22:23:31 +1100

    Add background

--- load.lua
+++ load.lua
@@ -5,6 +5,10 @@ function love.load()
   bullet = Bullet:new(nil, 'assets/weapons/red/laserRed01.png', {x = 1, y = 1}, 12)
   ship = Ship:new(nil, 'assets/ships/orange/ship2.png', {x = 0.45, y = 0.45}, 4.5, bullet)
 
+  bg = love.graphics.newImage("assets/backgrounds/blue.png")
+  bg:setWrap("repeat", "repeat")
+  bg_quad = love.graphics.newQuad(0, 0, love.graphics.getWidth(), love.graphics.getHeight(), bg:getWidth(), bg:getHeight())
+
   enemy = {
     x = 0,
     y = 0,
